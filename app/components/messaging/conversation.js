85180124e94091b042971a21fd9fe3e0fcb2bfbb
Nathan Greenberg <nathanjgreenberg@gmail.com>
2016-03-06 16:21:15 -0500

    added alert for no messages

--- app/components/messaging/conversation.js
+++ app/components/messaging/conversation.js
@@ -11,6 +11,22 @@ export default class Conversation extends React.Component {
       defaultTitle = data.messages[data.messages.length - 1].title;
     }
 
+    if (data.messages.length < 1) {
+      return (
+        <div className="conversation">
+          <h3 className="no-messages-alert">There are no messages in this conversation yet...</h3>
+
+          <form className="messaging-text-entry">
+            <fieldset className="form-group">
+              <textarea className="form-control text-entry-title" defaultValue={defaultTitle} rows="1" />
+              <textarea className="form-control text-entry-message" placeholder="Write a message..." rows="3" />
+            </fieldset>
+            <button type="submit" className="btn btn-primary pull-right">Submit</button>
+          </form>
+        </div>
+      )
+    }
+
     return (
       <div className="conversation">
         <div>
