aa8bf9e822babf93cb8449492a15f9f83bbcab14
Nathan Greenberg <nathanjgreenberg@gmail.com>
2016-03-06 14:13:57 -0500

    messaging css

--- app/components/messaging/conversation.js
+++ app/components/messaging/conversation.js
@@ -6,26 +6,20 @@ export default class Conversation extends React.Component {
   render() {
     var data = this.props.data;
     return (
-      <div>
-
+      <div className="conversation">
         {data.messages.map((message, i) => {
            return (
              <Message key={i} data={message} user={this.props.user} />
            )
          })}
 
-        <div className="row messaging-text-entry">
-          <div className="col-md-12">
-            <form>
-              <fieldset className="form-group">
-                <textarea className="form-control text-entry-title" defaultValue="Re: The Projectionist" rows="1" />
-                <textarea className="form-control text-entry-message" placeholder="Write a message..." rows="3"></textarea>
-              </fieldset>
-              <button type="submit" className="btn btn-primary pull-right">Submit</button>
-            </form>
-          </div>
-        </div>
-
+             <form className="messaging-text-entry">
+               <fieldset className="form-group">
+                 <textarea className="form-control text-entry-title" defaultValue="Re: The Projectionist" rows="1" />
+                 <textarea className="form-control text-entry-message" placeholder="Write a message..." rows="3"></textarea>
+               </fieldset>
+               <button type="submit" className="btn btn-primary pull-right">Submit</button>
+             </form>
       </div>
     )
   }
