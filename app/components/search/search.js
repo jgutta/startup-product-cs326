d31c8291878848892f339622c8d4820faa46cc66
Briennaa <sdobson1995@gmail.com>
2016-03-13 00:32:06 -0500

    fixing search bar lay out

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -63,13 +63,14 @@ export default class Search extends React.Component {
       <MainContent title= "UBoard Search">
         <div className="main-content">
           <div className="main-content-body">
-            <div className="bar pull-left">
-              <div className="input-group search-bar">
-                <span className="input-group-btn">
+
+            <div className="bar">
+              <div className="input-group">
                   <input type="text" className="form-control" placeholder="Search UBoard"
                     value={this.state.value} onChange={(e) => this.handleChange(e)}
                     onKeyUp={(e) => this.handleKeyUp(e)} />
-                  <button className="btn btn-default schbt" type="button" onClick={(e) => this.handleSubmit(e)}>
+                  <span className="input-group-btn">
+                  <button className="btn btn-default" onClick={(e) => this.handleSubmit(e)}>
                     <i className="fa fa-search"></i>
                   </button>
                 </span>
