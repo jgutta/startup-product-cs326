bf19ae0166d3703209746d8298a7f1ffbb98406d
Nathan Greenberg <nathanjgreenberg@gmail.com>
2016-04-05 15:15:18 -0400

    create-thread stuff

--- client/app/components/createthread/createthread.js
+++ client/app/components/createthread/createthread.js
@@ -57,8 +57,8 @@ export default class CreateThread extends React.Component {
     }
     else{
       var arrayvar = this.state.boards.slice()
-      arrayvar.push(e.target.value)
-      this.setState({ boards: arrayvar })
+        arrayvar.push(e.target.value)
+        this.setState({ boards: arrayvar })
     }
   }
 
@@ -69,7 +69,7 @@ export default class CreateThread extends React.Component {
 
     var threadTitle = this.state.title.trim();
     if(this.state.date!==null)
-        threadDate = this.state.date.format("l");
+      threadDate = this.state.date.format("l");
     var threadTime = this.state.time.trim();
     var threadDescription = this.state.description.trim();
     var threadImage = this.state.img.trim();
@@ -79,121 +79,95 @@ export default class CreateThread extends React.Component {
       alert("Missing thread title!");
     else if(threadDescription === "")
       alert("Missing thread description!")
-    else if(threadBoards.length === 0)
+      else if(threadBoards.length === 0)
       alert("You need to select at least one board!")
-    else if(threadBoards.length >= 5)
+      else if(threadBoards.length >= 5)
       alert("The max limit of boards is 4!")
-    else{
-      createThread(this.props.user, threadTitle, threadDate, threadTime, threadDescription, threadImage, threadBoards, (thread) => {
+      else{
+        createThread(this.props.user, threadTitle, threadDate, threadTime, threadDescription, threadImage, threadBoards, (thread) => {
           scrollTo(0,0);
           hashHistory.push({ pathname: '/threads/' + thread._id});
-      });
-    }
+        });
+      }
   }
 
   render() {
     return (
       <MainContent title="Create Thread">
-        <div classNameName="col-md-6">
-          <div className="panel panel-default p">
-            <div className="panel-body">
-              <form>
-                <div className="main-content">
-
-                  <div className="main-content-body">
-                    <span className="red">Fields marked with asterisk (*) are required</span>
-                    <div className="panel-body title form-group">
-                      <div className="media">
-                        <center>Title<font color="red">*</font>
-                        </center>
-                        <div className="media-body">
-                          <input type="text" className="form-control p" placeholder="" name="title" value={this.state.title} onChange={(e) => this.handleTitleChange(e)} required></input>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="panel-body date time title form-group">
-                      <div className="media">
-                        <div className="col-md-6">
-                          <center>Date</center>
-                          <div className="form-group">
-                            <center>
-                              <DatePicker placeholderText={moment().format("l")} todayButton="Today" minDate={moment()} selected={this.state.date} onChange={this.handleDateChange.bind(this)} />
-                            </center>
-                          </div>
-                        </div>
-                        <div className="col-md-6">
-                          <center>Time</center>
-                          <div className="form-group">
-                            <div className="input-group date" id="datetimepicker3">
-                              <input type='text' className="form-control p" placeholder={moment().format('LT')} name="time" value={this.state.time} onChange={(e) => this.handleTimeChange(e)}/>
-                              <span className="input-group-addon">
-                                <span className="glyphicon glyphicon-time"></span>
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="panel-body description title form-group">
-                      <div className="media">
-                        <div className="media-top">
-                          <center>Description<font color="red">*</font>
-                          </center>
-                        </div>
-                        <div className="media-body">
-                          <textarea className="form-control p" rows="7" placeholder="" name="description" value={this.state.description} onChange={(e) => this.handleDescriptionChange(e)} required></textarea>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="panel-body picture form-group">
-                      <a><img id="output" src={this.state.img} width="30%"/></a>
-                      <input type="file" className="browse" accept="image/jpeg, image/png" name="image" value={this.state.img} onChange={(e) => this.handleImageChange(e)}></input>
-                    </div>
-
-                    <div className="panel-body checkboxes form-group box">
-                      <div className="media">
-                        <div className="media-top title">
-                          <center>What Boards would you like to post this to?<font color="red">*</font>
-                          </center>
-                        </div>
-                        <ul className="checkbox-grid" onChange={(e) => this.handleBoardChange(e)}>
-                          <li><input type="checkbox" name="board" value="1" defaultChecked/>
-                            General</li>
-                          <li><input type="checkbox" name="board" value="2"/>
-                            Concerts</li>
-                          <li><input type="checkbox" name="board" value="3"/>
-                            Games</li>
-                          <li><input type="checkbox" name="board" value="4"/>
-                            Local Events</li>
-                          <li><input type="checkbox" name="board" value="5"/>
-                            Music</li>
-                          <li><input type="checkbox" name="board" value="6"/>
-                            Notes/Textbooks</li>
-                          <li><input type="checkbox" name="board" value="7"/>
-                            RSOs</li>
-                          <li><input type="checkbox" name="board" value="8"/>
-                            Sports</li>
-                          <li><input type="checkbox" name="board" value="9"/>
-                            Studying</li>
-                          <li><input type="checkbox" name="board" value="10"/>
-                            TV/Movies</li>
-                          <li><input type="checkbox" name="board" value="11"/>
-                            Videogames</li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                  <hr/>
-                  <div className="submit">
-                    <button type="submit" className="btn btn-primary" onClick={(e) => this.handleNewThread(e)}>Submit</button>
-                  </div>
-                </div>
-              </form>
+        <div className="row create-thread-row">
+          <div className="col-md-6">
+            <div className="input-box-title">Title</div>
+            <input type="text" className="form-control create-thread-input" name="title" value={this.state.title} onChange={(e) => this.handleTitleChange(e)} required></input>
+          </div>
+          
+          <div className="col-md-3">
+            <div className="input-box-title">Date</div>
+            <div className="input-group date-input">
+              <DatePicker placeholderText={moment().format("l")} todayButton="Today" minDate={moment()} selected={this.state.date} onChange={this.handleDateChange.bind(this)} />
+              <span className="input-group-addon">
+                <span className="glyphicon glyphicon-calendar"></span>
+              </span>
             </div>
           </div>
+          
+          <div className="col-md-3">
+            <div className="input-box-title">Time</div>
+            <div className="input-group time-input">
+              <input type='text' className="form-control create-thread-input" placeholder={moment().format('LT')} name="time" value={this.state.time} onChange={(e) => this.handleTimeChange(e)}/>
+              <span className="input-group-addon">
+                <span className="glyphicon glyphicon-time"></span>
+              </span>
+            </div>
+          </div>
+        </div>
+        
+        <div className="row create-thread-row">
+          <div className="col-md-6">
+            <div className="input-box-title">Description</div>
+            <textarea className="form-control description-input" rows="5" name="description" value={this.state.description} onChange={(e) => this.handleDescriptionChange(e)} required></textarea>
+          </div>
+          
+          <div className="col-md-1" />
+          
+          <div className="col-md-5">
+            <div className="input-box-title">Image</div>
+            <img className="image-input" id="output" src={this.state.img} />
+            <input type="file" className="browse" accept="image/jpeg, image/png" name="image" value={this.state.img} onChange={(e) => this.handleImageChange(e)} />
+          </div>
+        </div>
+        
+        <div className="row create-thread-row">
+          <div className="col-md-12">
+            <div className="input-box-title">Boards to post to</div>
+            <ul className="checkbox-grid" onChange={(e) => this.handleBoardChange(e)}>
+              <li><input type="checkbox" name="board" value="1"/>
+                General</li>
+              <li><input type="checkbox" name="board" value="2"/>
+                Concerts</li>
+              <li><input type="checkbox" name="board" value="3"/>
+                Games</li>
+              <li><input type="checkbox" name="board" value="4"/>
+                Local Events</li>
+              <li><input type="checkbox" name="board" value="5"/>
+                Music</li>
+              <li><input type="checkbox" name="board" value="6"/>
+                Notes/Textbooks</li>
+              <li><input type="checkbox" name="board" value="7"/>
+                RSOs</li>
+              <li><input type="checkbox" name="board" value="8"/>
+                Sports</li>
+              <li><input type="checkbox" name="board" value="9"/>
+                Studying</li>
+              <li><input type="checkbox" name="board" value="10"/>
+                TV/Movies</li>
+              <li><input type="checkbox" name="board" value="11"/>
+                Videogames</li>
+            </ul>
+          </div>
+        </div>
+        
+        <div className="create-thread-center">
+          <button type="submit" className="btn btn-primary" onClick={(e) => this.handleNewThread(e)}>Submit</button>
         </div>
       </MainContent>
     )
