b70c5b3dc14300770d12db28a35d641c789d90c0
Nathan Greenberg <nathanjgreenberg@gmail.com>
2016-04-05 16:35:26 -0400

    put subscribedboards route into its own file

--- server/src/server.js
+++ server/src/server.js
@@ -60,65 +60,11 @@ function getBoardData(boardId) {
 // /user/:userid/subscribedboards
 // ==========
 
-app.get('/user/:userid/subscribedBoards', function(req, res) {
-  var fromUser = getUserIdFromToken(req.get('Authorization'));
-  // Convert params from string to number.
-  var userId = parseInt(req.params.userid, 10);
-  if (fromUser === userId) {
-    var userData = readDocument('users', userId);
-    var subscribedBoards = {
-      contents: []
-    };
-    subscribedBoards.contents = userData.subscribedBoards.map(getBoardData);
-
-    res.send(subscribedBoards);
-  } else {
-    res.status(401).end();
-  }
-});
-
-app.put('/user/:userid/subscribedBoards/:boardid', function(req, res) {
-  var fromUser = getUserIdFromToken(req.get('Authorization'));
-
-  var userId = parseInt(req.params.userid, 10);
-  var boardId = parseInt(req.params.boardid, 10);
-
-  if (fromUser === userId) {
-    var userData = readDocument('users', userId);
-    userData.subscribedBoards.push(boardId);
-    writeDocument('users', userData);
-
-    res.send(userData);
-  } else {
-    res.status(401).end();
-  }
-});
-
-function getIndex(array, element) {
-  for(var i =0; i<array.length; i++){
-    if(array[i] == element){
-      return i;
-    }
-  }
-}
-
-app.delete('/user/:userid/subscribedBoards/:boardid', function(req, res) {
-  var fromUser = getUserIdFromToken(req.get('Authorization'));
-
-  var userId = parseInt(req.params.userid, 10);
-  var boardId = parseInt(req.params.boardid, 10);
-
-  if (fromUser === userId) {
-    var userData = readDocument('users', userId);
-    var index = getIndex(userData.subscribedBoards, boardId);
-    userData.subscribedBoards.splice(index, 1);
-    writeDocument('users', userData);
-
-    res.send(userData);
-  } else {
-    res.status(401).end();
-  }
-});
+require('./routes/subscribedboards.js').
+          setApp(app,
+                 getUserIdFromToken,
+                 readDocument, writeDocument,
+                 getBoardData);
 
 // ==========
 // /user/:userid/conversation
