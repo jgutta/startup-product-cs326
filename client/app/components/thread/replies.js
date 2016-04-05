e9305a9a95968de0012a786d9e5ae8c6a084f3d8
Nathan Greenberg <nathanjgreenberg@gmail.com>
2016-04-05 14:52:25 -0400

    thread reply css to save space

--- client/app/components/thread/replies.js
+++ client/app/components/thread/replies.js
@@ -8,7 +8,7 @@ export default class Replies extends React.Component {
     this.state = {
       editReply: false,
       messageContentsValue: ''
-     };
+    };
   }
 
   refresh(props) {
@@ -24,60 +24,65 @@ export default class Replies extends React.Component {
 
   componentWillReceiveProps(nextProps){
     this.refresh(nextProps);
-}
+  }
 
-toggleReply(){
-  this.setState({editReply: !this.state.editReply});
-}
+  toggleReply(){
+    this.setState({editReply: !this.state.editReply});
+  }
 
-handleContentsChange(e) {
-  e.preventDefault();
-  this.setState({ messageContentsValue: e.target.value });
-}
+  handleContentsChange(e) {
+    e.preventDefault();
+    this.setState({ messageContentsValue: e.target.value });
+  }
 
-handleReply(e){
-  e.preventDefault();
-  //worry about the rest later, there are problems to fix
-}
+  handleReply(e){
+    e.preventDefault();
+    //worry about the rest later, there are problems to fix
+  }
 
   render() {
-     var data = this.props.data;
+    var data = this.props.data;
 
-     return(
-       <ul className="media-list reply-list">
-         {data.map((reply) => {
-             return (
-               <li className="media" key={reply._id}>
-                 <div className="media-left">
-                  <img className="media-object img-rounded" src={reply.authorImage} />
-                 </div>
-                 <div className="media-body">
-                  {reply.contents}
-                  <hr />
-                  Posted by {reply.authorUsername} on {unixTimeToString(reply.postDate)}
-                  <button type="button" className="btn btn-primary reply-btn" onClick={this.toggleReply.bind(this)}> Reply </button>
+    return(
+      <ul className="media-list reply-list">
+        {data.map((reply) => {
+           return (
+             <li className="media" key={reply._id}>
+               <div className="media-left">
+                 <img className="media-object img-rounded" src={reply.authorImage} />
+               </div>
+               <div className="media-body">
+                 {reply.contents}
 
-                  <div className="toggleReplyArea">
-                    
-                  <h1>{this.state.togglReply}</h1>
-                  {this.state.editReply
+                 <hr />
+
+                 <span className="reply-data">
+                   Posted by {reply.authorUsername} on {unixTimeToString(reply.postDate)}
+                 </span>
+
+                 <button type="button" className="btn btn-primary reply-btn" onClick={this.toggleReply.bind(this)}> Reply </button>
+
+                 <div className="toggleReplyArea">
+
+                   {this.state.togglReply}
+                   {this.state.editReply
                     ? <div>
-                      <br />
-                      <textarea className="reply-box" rows="2" placeholder={'Reply to this post'}  onChange={(e) => this.handleContentsChange(e)} />
-                      <button type="button" className="btn btn-primary rep-btn" onClick={(e) => this.handleReply(e)}> Submit </button>
-                      </div>
+                     <br />
+                     <textarea className="reply-box" rows="2" placeholder={'Reply to this post'}  onChange={(e) => this.handleContentsChange(e)} />
+                     <button type="button" className="btn btn-primary rep-btn" onClick={(e) => this.handleReply(e)}> Submit </button>
+                    </div>
 
                     : <div> </div>
-                  }
-                  </div>
-
-                   <Replies data={reply.replies} />
+                   }
                  </div>
-              </li>
-            );
-          })}
-       </ul>
-     )
-   }
+
+                 <Replies data={reply.replies} />
+               </div>
+             </li>
+           );
+         })}
+      </ul>
+    )
+  }
 
 }
