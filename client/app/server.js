3c5ec4a6e037a5f71c2a90ccd7cf91f8d3e78569
reiffev <reiffev@gmail.com>
2016-04-07 16:28:23 -0400

    Webpage loads again

--- client/app/server.js
+++ client/app/server.js
@@ -1,7 +1,3 @@
-import { readDocument, writeDocument, addDocument, readCollection } from './database.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 var token = 'eyJpZCI6MX0='; // <-- Put your base64'd JSON token here
 /**
  * Properly configure+send an XMLHttpRequest with error handling, authorization token,
@@ -421,29 +417,3 @@ export function getPinned(user, cb) {
     cb(JSON.parse(xhr.responseText));
   });
 }
-
-
-
-/**
- * Reset database button.
- */
-export class ResetDatabase extends React.Component {
-  render() {
-    return (
-      <button className="btn btn-default" type="button" onClick={() => {
-          var xhr = new XMLHttpRequest();
-          xhr.open('POST', '/resetdb');
-          xhr.addEventListener('load', function() {
-            window.alert("Database reset! Refreshing the page now...");
-            document.location.reload(false);
-          });
-          xhr.send();
-        }}>Reset Mock DB</button>
-    );
-  }
-}
-
-ReactDOM.render(
-  <ResetDatabase />,
-  document.getElementById('db-reset')
-);
