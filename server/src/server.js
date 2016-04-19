910c08616163a1f0b0f974adc0a42ce4a84d1571
Nathan Greenberg <nathanjgreenberg@gmail.com>
2016-04-19 17:19:49 -0400

    get feed from database

--- server/src/server.js
+++ server/src/server.js
@@ -56,21 +56,47 @@ MongoClient.connect(url, function(err, db) {
     }
   }
 
+  function getUser(userId, callback) {
+    db.collection('users').findOne({
+      _id: userId
+    }, function(err, user) {
+      if (err) {
+        return callback(err);
+      } else if (user === null) {
+        return callback(null, null);
+      }
+
+      callback(null, user)
+    });
+  }
 
 
-
-// ===================
-// /board/
-app.get('/board/:boardId', function(req, res){
-  var board = readDocument('boards', req.params.boardId);
-  board.threads = board.threads.map((id) => getThreadSync(id));
-//  board.threads.originalPost.author = board.threads.originalPost.author.map((id) => readDocument('users', id)); Needs Testing
-  res.send(board);
-});
+  // ===================
+  // /board/
+  app.get('/board/:boardId', function(req, res){
+    var board = readDocument('boards', req.params.boardId);
+    board.threads = board.threads.map((id) => getThreadSync(id));
+    //  board.threads.originalPost.author = board.threads.originalPost.author.map((id) => readDocument('users', id)); Needs Testing
+    res.send(board);
+  });
 
   function getThreadSync(threadId) {
     var thread = readDocument('threads', threadId);
     return thread;
+  }
+
+  function getThread(threadId, callback) {
+    db.collection('threads').findOne({
+      _id: threadId
+    }, function(err, thread) {
+      if (err) {
+        return callback(err);
+      } else if (thread === null) {
+        return callback(null, null);
+      }
+
+      callback(null, thread)
+    });
   }
 
   require('./routes/boards.js').
@@ -145,7 +171,74 @@ app.get('/board/:boardId', function(req, res){
   // /feed
   // ==========
 
+  function getFeedData(feedId, callback) {
+    db.collection('feeds').findOne({
+      _id: feedId
+    }, function(err, feedData) {
+      if (err) {
+        return callback(err);
+      } else if (feedData === null) {
+        return callback(null, null);
+      }
+
+      // We will place all of the resolved FeedItems here.
+      // When done, we will put them into the Feed object
+      // and send the Feed to the client.
+      var resolvedContents = [];
+
+      // processNextFeedItem is like an asynchronous for loop:
+      // It performs processing on one feed item, and then triggers
+      // processing the next item once the first one completes.
+      // When all of the feed items are processed, it completes
+      // a final action: Sending the response to the client.
+      function processNextThread(i) {
+        // Asynchronously resolve a feed item.
+        getThread(feedData.contents[i], function(err, thread) {
+          if (err) {
+            // Pass an error to the callback.
+            callback(err);
+          } else {
+            // Success!
+            resolvedContents.push(thread);
+            if (resolvedContents.length === feedData.contents.length) {
+              // I am the final feed item; all others are resolved.
+              // Pass the resolved feed document back to the callback.
+              feedData.contents = resolvedContents;
+              callback(null, feedData);
+            } else {
+              // Process the next feed item.
+              processNextThread(i + 1);
+            }
+          }
+        });
+      }
+
+      // Special case: Feed is empty.
+      if (feedData.contents.length === 0) {
+        callback(null, feedData);
+      } else {
+        processNextThread(0);
+      }
+    });
+  }
+
+
   app.get('/feed/:userid/', function(req, res) {
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    var userId = req.params.userid;
+    if (fromUser === userId || userId === '000000000000000000000002') {
+      getUser(new ObjectID(userId), function(err, user) {
+        getFeedData(user.feed, function(err, feedData) {
+          res.send(feedData);
+        });
+      });
+    } else {
+      res.status(401).end();
+    }
+  });
+
+
+  /*app.get('/feed/:userid/', function(req, res) {
     var fromUser = getUserIdFromToken(req.get('Authorization'));
     // Convert params from string to number.
     var userId = req.params.userid;
@@ -159,7 +252,7 @@ app.get('/board/:boardId', function(req, res){
     } else {
       res.status(401).end();
     }
-  });
+  });*/
 
   // ==========
   // /search
